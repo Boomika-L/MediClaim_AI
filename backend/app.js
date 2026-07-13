47dcc567753470424364ae4e282562977ec2c8c0
Boomika-L <boomika.l2024cse@sece.ac.in>
2026-07-13 12:04:20 +0530

    authentication module for user

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Default Route
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Medical Insurance Prediction API is Running"
-    });
+  res.json({
+    success: true,
+    message: "Medical Insurance Prediction API Running",
+  });
 });
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
